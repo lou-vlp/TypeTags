1f36de5aca3a5e9d0233308d0f7cfa1baa48b741
Lou Andreev <lou.valpen@gmail.com>
2024-07-23 03:19:31 +0200

    cleanup

---
+++ Interpolation.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// Rely on std::lerp for floating-point types
+template <typename T> requires std::floating_point<T>
+struct Interpolant<T>
+{
+    static T Lerp(const T& x, const T& y, float t) noexcept
+    {
+        return std::lerp(x, y, t);
+    }
+};
+
+// Special treatment for ints
+template<>
+struct Interpolant<int>
+{
+    static int Lerp(const int& x, const int& y, float t) noexcept
+    {
+        return static_cast<int>(t * static_cast<float>(y - x) + float(x));
+    }
+};
+
+// Enforce globally consistent type indices
+const InitGlobalTypeIndices<int, float, double> KeyframableTypes;
